9977a6c6f673241748eedb52bb81614771e27bdc
DevBO7MED <mamd245r@gmail.com>
2024-08-11 13:02:29 -0400

    Update files

--- install.sh
+++ install.sh
@@ -1,28 +1,22 @@
 #!/bin/sh
 
+echo " ___ _   _ ____ _____  _    _     _     ___ _   _  ____"
+echo "|_ _| \ | / ___|_   _|/ \  | |   | |   |_ _| \ | |/ ___| "
+echo " | ||  \| \___ \ | | / _ \ | |   | |    | ||  \| | |  _   _    _    _"
+echo " | || |\  |___) || |/ ___ \| |___| |___ | || |\  | |_| | (_)  (_)  (_)"
+echo "|___|_| \_|____/ |_/_/   \_\_____|_____|___|_| \_|\____| "
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root" 1>&2
     exit 1
 fi
 
-<<<<<<< HEAD
 echo "Starting installation..."
 echo
 echo
 sudo apt-get update
+echo
 sudo apt-get install -y figlet lolcat
 echo
 echo
 echo "Installation complete! Now You Can Run The Script"
-=======
-
-apt-get update
-apt-get install -y figlet lolcat
-
-echo "Installation complete!"
->>>>>>> origin/main
